6c0ecaa4321804b9359d4d207c84a1ab1cad9f57
Prabhat Tripathi <pratripa7@WKMZTE55DB1E.global.publicisgroupe.net>
2023-02-16 11:40:09 +0530

    login,signup ui added

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,4 +1,53 @@
+import {
+  LoginContainer,
+  TextContent,
+  Form,
+  FormInput,
+  LoginButton,
+} from "./Register.styled";
 const Register = () => {
-  return <>Register works</>;
+  return (
+    <>
+      <LoginContainer>
+        <TextContent>
+          <h2>Signup</h2>
+          <p>We do not share your personal information with anyone</p>
+        </TextContent>
+        <Form>
+          <FormInput>
+            <label htmlFor="first">
+              First Name<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="text" id="first" />
+          </FormInput>
+          <FormInput>
+            <label htmlFor="last">
+              Last Name<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="text" id="last" />
+          </FormInput>
+          <FormInput>
+            <label htmlFor="email">
+              Email<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="text" id="email" />
+          </FormInput>
+          <FormInput>
+            <label htmlFor="password">
+              Password<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="password" id="password" />
+          </FormInput>
+          <FormInput>
+            <label htmlFor="conPass">
+              Confirm Password<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="password" id="conPass" />
+          </FormInput>
+          <LoginButton type="submit">Signup</LoginButton>
+        </Form>
+      </LoginContainer>
+    </>
+  );
 };
 export default Register;
--- client/src/pages/SignIn/Signin.jsx
+++ client/src/pages/SignIn/Signin.jsx
@@ -1,4 +1,35 @@
+import {
+  LoginContainer,
+  TextContent,
+  Form,
+  FormInput,
+  LoginButton,
+} from "./Signin.styled";
 const SignIn = () => {
-  return <>Signin Works</>;
+  return (
+    <>
+      <LoginContainer>
+        <TextContent>
+          <h2>Login</h2>
+          <p>Get access to your Orders, Wishlist and Recommendations</p>
+        </TextContent>
+        <Form>
+          <FormInput>
+            <label htmlFor="email">
+              Email<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="email" id="email" />
+          </FormInput>
+          <FormInput>
+            <label htmlFor="password">
+              Password<span aria-hidden="true">*</span>
+            </label>
+            <input required="true" type="password" id="password" />
+          </FormInput>
+          <LoginButton type="submit">Login</LoginButton>
+        </Form>
+      </LoginContainer>
+    </>
+  );
 };
 export default SignIn;
