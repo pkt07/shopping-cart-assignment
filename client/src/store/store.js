fff141825e323f258c69d756025ebfe775ad70ba
Prabhat Tripathi <pratripa7@WKMZTE55DB1E.global.publicisgroupe.net>
2023-02-21 10:24:38 +0530

    product store reducer function

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cart/cartSlice";
+import productSlice from "./products/productSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
+    product: productSlice,
   },
 });
